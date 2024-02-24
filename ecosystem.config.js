2724de952c8e3b7a63f4ed17f9b11182879ee0e2
Ilona Beshchuk <33685765+illonab@users.noreply.github.com>
2024-02-24 22:42:52 +0000

    Fix for https-server pm2 ecosystem item to actually use http-server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: "https-server",
-        script: "./app.js",
+        script: "http-server",
         args: "-S -b -d false -C ./ssl/certificate.pem -K ./ssl/certificate.key --proxy https://${HOST}/? -p 443 ./client/build/"
     }, {
         name: "api-server",
